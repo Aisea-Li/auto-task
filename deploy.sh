2197e44caa63810f33469611f9a6deb191f4746e
Aisea <aisea.l@mexc.com>
2024-02-17 13:07:04 +0800

    优化脚本

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,20 @@ git pull
 
 mvn clean package -Dmaven.test.skip=true
 
+if [ -f "/app/auto-task/stop.sh" ]
+then
+    sh /app/auto-task/stop.sh
+fi
+
+rm -rf /app/auto-task/
+
+mkdir /app/auto-task/
+
 mv ./target/auto-task.jar /app/auto-task/auto-task.jar
 
 mv ./start.sh /app/auto-task/start.sh
+
+mv ./stop.sh /app/auto-task/stop.sh
 
 cd /app/auto-task/
 
